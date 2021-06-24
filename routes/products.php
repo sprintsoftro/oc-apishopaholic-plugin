9ac9dfe585365aa1a28a6fbaf5caf939417e3795
Octavian Popa <octavian.popa@sprintsoft.ro>
2021-06-24 16:54:07 +0200

    add filter-list route

--- routes/products.php
+++ routes/products.php
@@ -5,6 +5,7 @@ Route::prefix('products')
         function () {
             Route::get('list', 'Products@list')->name('list');
             Route::get('{id}/offers', 'Products@offers')->name('offers');
+            Route::get('{category}/filter-list', 'Products@filterList')->name('filterList');
         }
     );
 Route::apiResource('products', 'Products', ['only' => ['index', 'show']]);
